0606b5d6087ac166571dc9481a9fea2556517fa6
wps13 <willanepaiva@hotmail.com>
2018-06-02 09:23:29 -0300

    chal. 15 done

--- Challenge 15/start/script.js
+++ Challenge 15/start/script.js
@@ -12,6 +12,10 @@ var vowels = ["a", "e", "i", "o", "u"];
 var str = "Today is best day of my life";
 
 // Write code here
-
+    for(let i=0; i < str.length ; i++){
+        if(str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90) 
+        vowelsCount++ 
+    }  
+ 
 console.log(vowelsCount);
 // 8
